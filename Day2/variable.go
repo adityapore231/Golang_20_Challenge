a9f265698ea07df566b42f0e83d28044f862c451
adityapore <adityapore001@gmail.com>
2024-02-05 16:03:22 +0530

    latest

---
+++ Day2/variable.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "aditya"
+	fmt.Println(username)
+	fmt.Printf("type of varible is %T \n", username)
+
+	var onoff bool = true
+	fmt.Println(onoff)
+	fmt.Printf("type of varible is %T \n", onoff)
+
+	var smallval uint8 = 245
+	fmt.Println(smallval)
+	fmt.Printf("type of varible is %T \n", smallval)
+
+	var floatval float32 = 245.4654576876
+	fmt.Println(floatval)
+	fmt.Printf("type of varible is %T \n", floatval)
+
+	//245.46545
+}
